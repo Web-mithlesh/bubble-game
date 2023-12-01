7e52400716ddf0575ce37bb43e1370806b525472
Mithlesh kumar Sahani <121002356+Web-mithlesh@users.noreply.github.com>
2023-12-01 13:47:22 +0530

    added script.js

---
+++ script.js
@@ -0,0 +1,53 @@
+var timer = 30;
+var score = 0;
+var hitrn = 0;
+
+function increaseScore(){
+    score += 10;
+    document.querySelector("#scoreval").textContent = score;
+}
+
+function getNewHit(){
+    hitrn = Math.floor(Math.random()*10);
+    document.querySelector("#hitval").textContent = hitrn;
+}
+
+function makeBubble(){
+    var clutter = "";
+
+for(var i=1; i<=175; i++){
+    var rn = Math.floor(Math.random()*10)
+    clutter+= `<div class="bubble">${rn}</div>`
+}
+
+document.querySelector("#pnlbtm").innerHTML = clutter;
+}
+
+document.querySelector("#pnlbtm").addEventListener("click",function(dets){
+    var clickednum = Number(dets.target.textContent);
+    if(clickednum === hitrn){
+        increaseScore();
+        makeBubble();
+        getNewHit();
+
+    }
+});
+
+
+
+function runtimer(){
+  var timerint =  setInterval(function(){
+        if(timer > 0){
+            timer--;
+            document.querySelector("#timerval").textContent = timer;
+        }else{
+            clearInterval(timerint);
+            document.querySelector("#pnlbtm").innerHTML = `<h1>Game Over </h1>`;
+        }
+ 
+  
+    },1000);
+}
+runtimer();
+makeBubble();
+getNewHit();
